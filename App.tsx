4c8cc3f70233d6b16543aed3c7a9fbd9c5aa8917
Jackson Taylor <jacksoneliastaylor@gmail.com>
2023-01-27 17:59:17 -0500

    V.A

--- App.tsx
+++ App.tsx
@@ -3,74 +3,84 @@ import { initializeApp } from "firebase/app";
 import { Button, StyleSheet, Text, View } from 'react-native';
 import Counter, { CounterData, CounterDocumentData, PartialCounterDocumentData } from "./components/Counter";
 import { addDoc, collection, doc, getDocs, getFirestore, query, setDoc } from 'firebase/firestore';
-import { useEffect, useState } from "react";
 import * as FirebaseCore from 'expo-firebase-core';
+import { QueryClient, QueryClientProvider, useMutation, useQuery, useQueryClient } from "react-query";
 
 initializeApp(
   FirebaseCore.DEFAULT_WEB_APP_OPTIONS as FirebaseCore.FirebaseOptions
 );
 
-export default function App() {
-  const [counters, setCounters] = useState<CounterData[]>([]);
-  const [loading, setLoading] = useState(true);
+export default function Providers() {
+  return (
+    <QueryClientProvider client={new QueryClient()} >
+      <App />
+    </QueryClientProvider>
+  );
+}
 
-  async function refreshCounters() {
-    setLoading(true);
-    setCounters(await fetchCounters());
-    setLoading(false);
-  }
+async function _decrement(counter: CounterData) {
+  await setDoc(doc(getFirestore(), 'counters', counter.id), {
+    count: Math.max(counter.count - 1, 0),
+  } as PartialCounterDocumentData, {
+    merge: true, // prevent this from causing future bugs
+  });
+}
 
-  useEffect(() => { refreshCounters(); }, []);
+async function _increment(counter: CounterData) {
+  await setDoc(doc(getFirestore(), 'counters', counter.id), {
+    count: counter.count + 1,
+  } as PartialCounterDocumentData, {
+    merge: true, // prevent this from causing future bugs
+  });
+}
 
-  async function decrement(counter: CounterData) {
-    await setDoc(doc(getFirestore(), 'counters', counter.id), {
-      count: Math.max(counter.count - 1, 0),
-    } as PartialCounterDocumentData, {
-      merge: true, // prevent this from causing future bugs
-    });
-    refreshCounters();
-  }
+async function _addCounter() {
+  await addDoc(collection(getFirestore(), 'counters'), {
+    count: 0,
+  } as CounterDocumentData);
+}
 
-  async function increment(counter: CounterData) {
-    await setDoc(doc(getFirestore(), 'counters', counter.id), {
-      count: counter.count + 1,
-    } as PartialCounterDocumentData, {
-      merge: true, // prevent this from causing future bugs
-    });
-    refreshCounters();
-  }
+async function fetchCounters(): Promise<CounterData[]> {
+  const counterCollection =
+    await getDocs(query(collection(getFirestore(), 'counters')));
+  return counterCollection.docs.map((doc) => ({
+    ...(doc.data() as CounterDocumentData),
+    id: doc.id,
+  }));
+}
 
-  async function addCounter() {
-    await addDoc(collection(getFirestore(), 'counters'), {
-      count: 0,
-    } as CounterDocumentData);
-    refreshCounters();
-  }
+function App() {
+  const queryClient = useQueryClient();
 
-  async function fetchCounters(): Promise<CounterData[]> {
-    const counterCollection =
-      await getDocs(query(collection(getFirestore(), 'counters')));
-    return counterCollection.docs.map((doc) => ({
-      ...(doc.data() as CounterDocumentData),
-      id: doc.id,
-    }));
-  }
+  const { data: counters, isLoading } = useQuery('counters', fetchCounters);
+
+  const { mutate: increment } = useMutation(_increment, {
+    onSuccess: () => queryClient.invalidateQueries('counters'),
+  });
+
+  const { mutate: decrement } = useMutation(_decrement, {
+    onSuccess: () => queryClient.invalidateQueries('counters'),
+  });
+
+  const { mutate: addCounter } = useMutation(_addCounter, {
+    onSuccess: () => queryClient.invalidateQueries('counters'),
+  });
 
   function render() {
-    if (loading && !counters) {
+    if (isLoading && !counters) {
       return (
         <Text>Loading...</Text>
       );
     }
     return (
       <>
-        {counters.map((counter) => <Counter
+        {counters!.map((counter) => <Counter
           key={counter.id}
           count={counter.count}
           decrement={() => decrement(counter)}
           increment={() => increment(counter)}
         />)}
-        <Button title="Add Counter" onPress={addCounter} />
+        <Button title="Add Counter" onPress={() => addCounter()} />
       </>
     );
   }
